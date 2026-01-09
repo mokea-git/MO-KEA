0dfe3a7bef55938be453201ee2502d86f44af032
codepark-git <easycoding2967@gmail.com>
2026-01-10 08:38:43 +0900

    fixed error

--- mokea-app/src/pages/Linena.jsx
+++ mokea-app/src/pages/Linena.jsx
@@ -43,7 +43,7 @@ function Linena() {
           <Link to="/" className="flex items-center gap-3">
             <img src={linenaLogo} alt="LINENA" className="w-10 h-10" />
             <span className="text-2xl font-bold bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent">
-              LINENA
+              MO-KEA 
             </span>
           </Link>
 
